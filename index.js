3a947924d4a8ff65776a45f3973db249a4ef84d6
dearshrewdwit <dearshrewdwit@gmail.com>
2022-05-26 16:43:31 +0100

    extensions

--- index.js
+++ index.js
@@ -4,34 +4,46 @@ const cors = require("cors")
 const app = express()
 const port = 3030
 
-let counter = 0
+let counters = {
+  cars: 0,
+  animals: 0
+}
 
 app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
-app.get("/counter", (req, res) => {
+app.get("/counter/:name", (req, res) => {
+  console.log(req.params)
+  const counter = counters[req.params.name]
   res.json({counter: counter})
 })
 
-app.post('/counter/increment', (req, res) => {
-  counter++
-  res.json({counter: counter})
+app.post('/counter/:name/increment', (req, res) => {
+  counters[req.params.name]++
+  res.json({counter: counters[req.params.name]})
 })
 
-app.post('/counter/decrement', (req, res) => {
-  counter--
-  res.json({counter: counter})
+app.post('/counter/:name/decrement', (req, res) => {
+  counters[req.params.name]--
+  res.json({counter: counters[req.params.name]})
 })
 
-app.post('/counter/double', (req, res) => {
-  counter*=2
-  res.json({counter: counter})
+app.post('/counter/:name/double', (req, res) => {
+  counters[req.params.name]*=2
+  res.json({counter: counters[req.params.name]})
 })
 
-app.delete('/counter', (req, res) => {
-  counter = 0
-  res.json({counter: counter})
+app.delete('/counter/:name', (req, res) => {
+  counters[req.params] = 0
+  res.json({counter: counters[req.params.name]})
+})
+
+app.put('/counter/:name', (req, res) => {
+  if (req.query.value) {
+    counters[req.params.name] = Number(req.query.value)
+  }
+  res.json({counter: counters[req.params.name]})
 })
 
 app.listen(port, () => {
